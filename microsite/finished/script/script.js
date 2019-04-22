8f8f10b700a2b1f45a98e0cecfbeab0bbbf60f29
jeongm <limjeongmin@wustl.edu>
2019-04-22 12:24:14 -0500

    finished microsite

---
+++ microsite/finished/script/script.js
@@ -0,0 +1,37 @@
+$('#hamburger-menu').click(()=>{
+    $("#hamburger-menu").css('color','transparent');
+    $("#mobile-nav-expand-expand").css('display','flex');
+});
+
+$('#close-menu').click(()=>{
+    $("#hamburger-menu").css('color','black');
+    $("#mobile-nav-expand-expand").css('display','none');
+});
+
+$('#mobile-nav-expand-expand').click(()=>{
+    $("#hamburger-menu").css('color','black');
+    $("#mobile-nav-expand-expand").css('display','none');
+});
+
+$(window).scroll(()=>{
+    let height = $(window).scrollTop();
+    if(height>300){
+        $("#go-top").css('display','block');
+    }else{
+        $("#go-top").css('display','none');
+    }
+});
+
+$(document).ready(()=> {
+    $("#go-top").click((e)=> {
+        e.preventDefault();
+        //How to jump to top of browser page (https://stackoverflow.com/a/4147118)
+        $('html, body').animate({ scrollTop: 0 }, 'slow'); 
+        return false;
+    });
+});
+
+$('#mobile-source').click((e)=>{
+    e.preventDefault();
+    window.location.href='Sources.html';
+})
